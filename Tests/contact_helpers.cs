29677fb611f0b9e2992ce672701784d3bac3ceb1
Jim Holmes <jim@iterativerose.com>
2014-06-26 10:48:44 -0700

    Helper class

---
+++ Tests/contact_helpers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.Models;
+using WebApi.Services;
+
+namespace Tests
+{
+    public class contact_helpers
+    {
+
+        static public int create_test_user()
+        {
+            ContactRepository _repository = new ContactRepository();
+            Contact test = new Contact();
+            test.Id = -1;
+            test.Region = "zzzzz";
+            test.Company = "1212121";
+            test.LName = "zyzyzyz";
+            test.FName = "jhsadfljhasfdlkjh";
+            return _repository.SaveContact(test );
+        }
+    }
+}
